5488b880d8e6806a387a2383708565771f059da3
Andjela Perlas <38106481+Andjela-Perlas@users.noreply.github.com>
2021-12-09 00:06:46 +0100

    Update Contact.php

--- app/models/Contact.php
+++ app/models/Contact.php
@@ -4,7 +4,6 @@ namespace app\models;
 
 class Contact {
     
-
     private $conn;
 
     public function __construct(Database $conn) {
@@ -13,11 +12,10 @@ class Contact {
     }
 
 
-
     public function getContact() { 
+        
         return $this->conn->executeQuery("SELECT * FROM contact");
     }
-
 
 
     public function AddContact($fullName, $email, $content) {
@@ -31,16 +29,12 @@ class Contact {
         $query = "INSERT INTO contact(fullName, email, content)
         VALUES(?,?,?);";
 
-
         $this->conn->executeNonGet($query, $contact);
     }
 
 
-
-
     public function deleteContact($contactID) {
+        
         return $this->conn->executeNonGet("DELETE FROM contact WHERE contactID = ?", [$contactID]);
-
     }
 }
-
