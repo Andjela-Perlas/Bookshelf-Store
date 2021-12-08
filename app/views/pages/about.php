87290bb8ec85f240cf83074430c0269d8242eaa1
Andjela Perlas <38106481+Andjela-Perlas@users.noreply.github.com>
2021-12-09 00:01:28 +0100

    Update about.php

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,11 +1,11 @@
 <div class="container ">
-	<h1 class="my-5 font-weight-normal">Informacije o autoru</h1>
+	<h1 class="my-5 font-weight-normal">O nama</h1>
 	<div class="row">
-		<div class="col-md-7">
-			<img class="img-fluid img-thumbnail h-75" src="public/images/about.jpg" alt="author">
+		<div class="col-md-6 mb-5">
+			<img class="img-fluid img-thumbnail h-100 w-100 " src="public/images/about2.jpg" alt="author">
 		</div>
 		<div class="col-md-5">
-			<p>Zdravo, zovem se Anđela Perlaš i živim u Pančevu. Završila sam srednju ekonomsko-trgovinsku školu "Paja Marganović" u Pančevu. Trenutno sam završna godina Visoke ICT škole, smer Internet tehnologije.</p>
+			<p>Zdravo, BOOKSHELF već dugi niz godina sarađuje sa najznačajnijim domaćim i stranim distributerima i naučnim institucijama. Ukoliko ste zainteresovani za saradnju, tim naših stručnjaka će Vam rado izaći u susret, i proslediti Vam sve dalje informacije.</p>
 		</div>
 	</div>
 </div>
